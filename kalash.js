2da667e7d5c13920cf16b45d11fecec644615cc3
RoyalCube <73716013+CookieTriste@users.noreply.github.com>
2023-02-19 13:11:15 +0000

    Initial commit

---
+++ kalash.js
@@ -0,0 +1,15 @@
+module.exports = {
+  token: process.env.token,
+  port: "3000",
+  prefix: ">",
+  client: "",
+  client_id: "1076554108774916167",
+  client_secret: "3BNp575OvduZodnf9Tf37TUFNY2FobeO",
+  redirect_uri: "https://Auth-bot-Credit-BossTeam-1.kodelapatate.repl.co",
+  footer: "GemiPass",
+  support: "https://discord.gg/CMk5WDjjh2",
+  wehbook: "https://discord.com/api/webhooks/1076850979447779458/85swYDA4MTlc02P5UMli6SZuWo2d8FKEv09zB73jPgMFYZEBx-ccaiYjL_mjcnUYuJcz", //wehbook de logs
+  owners: ["1066277326108762183"],
+  authLink: `https://discord.com/api/oauth2/authorize?client_id=1076554108774916167&redirect_uri=https%3A%2F%2FAuth-bot-Credit-BossTeam-1.kodelapatate.repl.co&response_type=code&scope=guilds.join%20identify`,
+
+}
